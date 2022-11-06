74326109b27501eab750547e9d5c9b87d9c05547
ArtemRe <RechembeiArtem@gmail.com>
2022-11-06 21:32:45 +0200

    ++

--- src/components/PhoneBook/ContactList/ContactList.jsx
+++ src/components/PhoneBook/ContactList/ContactList.jsx
@@ -1,11 +1,20 @@
 
 import PropTypes from 'prop-types';
-export default function PhoneBookList({ items, removeContact }) {
+import { useDispatch } from 'react-redux';
+import { removeContact } from 'redux/itemsSlice';
+export default function PhoneBookList({ items }) {
+  const dispatch = useDispatch();
+
+  const onRemoveContact = id => {
+    const action = removeContact(id);
+    dispatch(action);
+  };
+
   const elements = items.map(({ name, number, id }) => {
     return (
       <li key={id}>
         {name}: {number}{' '}
-        <button type="submit" onClick={() => removeContact(id)}>
+        <button type="submit" onClick={() => onRemoveContact(id)}>
           Delete
         </button>
       </li>
